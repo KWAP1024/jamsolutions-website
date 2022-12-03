e6219692a5b2fd1fc21fdf83c3597e29640985fc
JastonRuggy <jastonruggy@icloud.com>
2022-12-03 19:45:25 +0300

    hye

--- database/connect.php
+++ database/connect.php
@@ -1,3 +1,6 @@
 <?php
+// Comment these configurations when online
 $conn = mysqli_connect('localhost', 'root', '', 'jam_solutions');
+
+// Uncomment these configurations when online
 // $conn = mysqli_connect('localhost', 'u830142481_jamsolutions', '', 'u830142481_jamsolutions');
